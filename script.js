e375bc9ce18e2b3157d8927585a17b891cfcfb3a
Mauricio Oliveira <99220549+mauriciomholiveira@users.noreply.github.com>
2023-12-14 18:15:16 -0300

    retornado ao padrao

--- script.js
+++
@@ -1,7 +0,0 @@
-.section .grid-container {
-    display: none; /* Esconder o conteúdo por padrão */
-}
-
-.section.expanded .grid-container {
-    display: grid; /* Mostrar o conteúdo quando a seção estiver expandida */
-}
